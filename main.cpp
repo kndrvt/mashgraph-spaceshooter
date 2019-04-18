dfaa21094a683c3d00e67f76d2b000cfadeaeacb
Kondrat <pantyuhin_lev@mail.ru>
2019-04-19 01:12:18 +0300

    ver. 1.1

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 //External dependencies
 #include <GLFW/glfw3.h>
-#include <SOIL.h>
+#include "SOIL.h"
 #include <random>
 
 static const GLsizei WIDTH = 640, HEIGHT = 480; //размеры окна
@@ -99,7 +99,7 @@ int main(int argc, char **argv) {
 
         glBindTexture(GL_TEXTURE_2D, texture);
         int width, height;
-        unsigned char* image = SOIL_load_image("container.jpg", &width, &height, 0, SOIL_LOAD_RGB);
+        unsigned char* image = SOIL_load_image("textures/wall.jpeg", &width, &height, 0, SOIL_LOAD_RGB);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
         glGenerateMipmap(GL_TEXTURE_2D);
         SOIL_free_image_data(image);
